8a36d12f56406e9c0aab0eca1a2a891f88562a18
madsr2d2 <mads.richardt@gmail.com>
2022-11-04 10:14:57 +0100

    mads

--- assignments/a5/src/main.c
+++ assignments/a5/src/main.c
@@ -1,14 +1,90 @@
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Function declarations
+void Diceman();
+void StackTrace();
 
 int main() {
-	srand(time(0));
-	int n;
+	puts("***********************");
+	puts("Welcome to Assignment 5");
+	puts("***********************");
 
-	for (size_t i = 0; i < 10; i++)
-	{
-		
-		n = rand() % 6 +1;
+	int selectionVariable = 0;
+
+	while (selectionVariable != 4) {
+	
+		printf("\n1: Diceman.\n2: Stack Trace.\n3: Factorial.\n4: Close Program.\nPlease choose option: ");
+		scanf("%1d",&selectionVariable);
+
+		switch (selectionVariable)
+		{
+		case 1:
+			puts("");
+			puts("*******");
+			puts("Diceman");
+			puts("*******");
+
+			printf("Rolling dice... ");
+			Diceman();
+			break;
+
+		case 2:
+			puts("");
+			puts("*******");
+			puts("Stack Trace");
+			puts("*******");
+			StackTrace();
+
+		default:
+			break;
+		}
+
 	}
+
+	puts("\nClosing Program...");
+}
+
+void Diceman() {
+	char action [100];
+
+	// Role dice
+	srand(time(0));
+	int dice = rand() % 6 + 1;
+	
+	switch (dice)
+	{
+	case 1:
+		strcpy(action,"Breakfast!");
+		printf("%s\n", action);
+		break;
+	case 2:
+		strcpy(action, "Study!");
+		printf("%s\n", action);
+		break;
+	case 3:
+		strcpy(action, "Swim!");
+		printf("%s\n", action);
+		break;
+	case 4:
+		strcpy(action, "Go fishing!");
+		printf("%s\n", action);
+		break;
+	case 5:
+		strcpy(action, "Call mom!");
+		printf("%s\n", action);
+		break;
+	case 6:
+		strcpy(action, "Back to bed!");
+		printf("%s\n", action);
+		break;
+	default:
+		break;
+	}
+}
+
+void StackTrace() {
 	
 }
